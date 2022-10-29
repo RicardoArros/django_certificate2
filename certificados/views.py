1f12df18d2786b62c33872dac74f479acc8b3aa9
RicardoArros <ricardo.arros@u-payments.com>
2022-10-29 20:31:40 -0300

    feat: added validation to create funct

--- certificados/views.py
+++ certificados/views.py
@@ -1,9 +1,7 @@
 from inspect import ArgSpec
-
 from django.shortcuts import render
-
 from sqlite3 import IntegrityError
-# from msilib.schema import Error
+#from msilib.schema import Error
 
 from .models import Certificado
 
@@ -11,9 +9,13 @@ from .models import Certificado
 def inicio(request):
   return render(request,"index.html")
 
+
+
 #
 def ingresar(request):
   return render(request,"ingresar.html")
+
+
 
 #
 def registro(request):
@@ -29,43 +31,66 @@ def registro(request):
   id_verificacion = request.POST['id_verificacion']
   
   #
- 
-  Certificado.objects.create(id_certificado=id_certificado, nombre=nombre, fecha=fecha, curso=curso, version=version, id_verificacion=id_verificacion)
-    
-  message = 'se ha ingresado el certificado'
-    
-          
-  return render(request,"registro.html", {'message': message})
-  # except Error as err:
-  #   message = f'ha ocurrido un problema en la operación_, {err}' 
+  try:
+    Certificado.objects.create(id_certificado=id_certificado, nombre=nombre, fecha=fecha, curso=curso, version=version, id_verificacion=id_verificacion)    
+    message = 'se ha ingresado el certificado' 
+
+  except Exception as ex:
+    if str(ex.__cause__).find('prueba2_certificado.id_certificado') > 0:
+      message = 'ha ocurrido un problema en la operación, id ya ingresado'
       
+    elif str(ex.__cause__).find('prueba2_certificado.id_verificacion') > 0:
+      message = 'ha ocurrido un problema en la operación, id ya ingresado'
+      
+    else:
+      message = 'ha ocurrido un problema en la operación'
+  
+  except Error as err:
+    message = f'ha ocurrido un problema en la operación_, {err}'
+
+  #
+  return render(request,"registro.html", {'message': message})
+
+
 
 #
 def actualizar(request):
   return render(request,"actualizar.html")
 
+
+
 #
 def editar(request):
-  #realizar actualizar
+  #
   pass
+  
+
 
 #
 def listar(request):
   certificados = Certificado.objects.all()
   return render(request, "listar.html", {'certificados': certificados})
 
+
+
 #
 def validar(request):
   return render(request,"validar.html")
+
+
 
 #
 def valida(request):
   #realizar valida
   pass
 
+
+
 #
 def eliminar(request):
   return render(request,"eliminar.html")
+
+
 
 #
 def elimina(request):
